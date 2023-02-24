7376179ad7d8e6ed71b66581123f6714fe4b910a
j-hyerim <112531998+j-hyerim@users.noreply.github.com>
2023-02-24 18:01:00 +0900

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,129 @@
+// function checkMobile(){
+ 
+//     var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+ 
+//     if ( varUA.indexOf('android') > -1) {
+//         //안드로이드
+//         alert("안드로이드임");
+//         return "android";
+//     } else if ( varUA.indexOf("iphone") > -1||varUA.indexOf("ipad") > -1||varUA.indexOf("ipod") > -1 ) {
+//         //IOS
+//         alert("아이폰임");
+//         return "ios";
+//     } else {
+//         //아이폰, 안드로이드 외
+//         return "other";
+//     }
+    
+// }
+// var mobile = (/iphone|ipad|ipod|android/i.test(navigator.userAgent.toLowerCase()));
+ 
+// if (mobile) { 
+//   //모바일 처리
+// }else{
+//   //비 모바일 처리
+// }
+// $(document).ready(function(){
+
+    
+    // $("div").click(function(){
+    //     // alert("test");
+    //     console.log("tttt");
+
+    //     var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+    
+    //     if ( varUA.indexOf('android') > -1) {
+    //         //안드로이드;
+    //         console.log("android");
+    //         alert("안드로이드임");
+    //         return "android";
+    //     } else if ( varUA.indexOf("iphone") > -1||varUA.indexOf("ipad") > -1||varUA.indexOf("ipod") > -1 ) {
+    //         //IOS
+    //         console.log("ios");
+    //         alert("아이폰임");
+    //         return "ios";
+    //     } else {
+    //         //아이폰, 안드로이드 외
+    //         return "other";
+    //     }
+            
+    // });
+// });
+
+
+// $(document).ready(function(){
+// $("div").click(function(){
+//     // alert("test");
+//     console.log("tttt");
+//     checkMobile();
+
+//     function checkMobile(){
+
+//         var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+    
+//         if ( varUA.indexOf('android') > -1) {
+//             //안드로이드;
+//             console.log("android");
+//             alert("안드로이드임");
+//             return "android";
+//         } else if ( varUA.indexOf("iphone") > -1||varUA.indexOf("ipad") > -1||varUA.indexOf("ipod") > -1 ) {
+//             //IOS
+//             console.log("ios");
+//             alert("아이폰임");
+//             return "ios";
+//         } else {
+//             //아이폰, 안드로이드 외
+//             return "other";
+//         }
+        
+//     }
+// });
+// });
+
+// var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+            
+// if ( varUA.indexOf('android') > -1) {
+//     //안드로이드;
+//     console.log("android");
+//     // alert("안드로이드임");
+//     // location.href = 'https://play.google.com/store/apps/details?id=cc.shoplink.mobile';
+//     return "android";
+// } else if ( varUA.indexOf("iphone") > -1||varUA.indexOf("ipad") > -1||varUA.indexOf("ipod") > -1 ) {
+//     //IOS
+//     console.log("ios");
+//     // alert("아이폰임");
+//     // location.href = 'https://apps.apple.com/kr/app/%EC%9C%99%EC%9E%87-wing-eat/id1219627913';
+//     return "ios";
+// } else {
+//     //아이폰, 안드로이드 외
+//     // return "other";
+//     // console.log("n");
+//     // location.href = 'https://play.google.com/store/apps/details?id=cc.shoplink.mobile';
+//     alert("컴퓨터주제에");
+// }
+    
+        // $("div").click(function(){
+        //     // alert("test");
+        //     console.log("tttt");
+
+        //     var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+
+        //     if ( varUA.indexOf('android') > -1) {
+        //         //안드로이드;
+        //         console.log("android");
+        //         alert("안드로이드임");
+        //         // return "android";
+        //     } else if ( varUA.indexOf("iphone") > -1||varUA.indexOf("ipad") > -1||varUA.indexOf("ipod") > -1 ) {
+        //         //IOS
+        //         console.log("ios");
+        //         alert("아이폰임");
+        //         // return "ios";
+        //     } else {
+        //         //아이폰, 안드로이드 외
+        //         // return "other";
+        //         console.log("n");
+        //         alert("컴퓨터주제에");
+        //     }
+                
+        // });
+    // });
